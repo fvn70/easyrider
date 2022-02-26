dc5f5a9f08b132118e27aa4e625da3c556aa86b7
fvn70 <fvn_reg@mail.ru>
2022-02-26 13:10:33 +0700

    Stage 4/6

--- Easy Rider Bus Company/task/easyrider/easyrider.py
+++ Easy Rider Bus Company/task/easyrider/easyrider.py
@@ -1,42 +1,28 @@
 import json
-import re
-
-
-def check(k, v):
-    if k in ('bus_id', 'stop_id', 'next_stop'):
-        return type(v) is int
-    if k == 'stop_name':
-        if v == '' or type(v) != type('a'):
-            return False
-        sn = v.split()
-        rez = len(sn) > 1 and sn[0][0].isupper() and sn[-1] in suff
-        return rez
-    if k == 'stop_type':
-        rez = type(v) == type('a') and v == '' or v in ['S', 'O', 'F']
-        return rez
-    if k == 'a_time':
-        return type(v) == type('a') and re.match(r'([0][1-9]|[1-2][0-9]):[0-5]\d$', v) != None
-    return True
-
-
-suff = 'Avenue Boulevard Road Street'
-errs = {"bus_id": 0, "stop_id": 0, "stop_name": 0, "next_stop": 0, "stop_type": 0, "a_time": 0}
-lines = {}
 
 
 def main():
     json_str = input()
     dics = json.loads(json_str)
-    for el in dics:
-        for k, v in el.items():
-            if k == 'bus_id':
-                if v in lines:
-                    lines[v] += 1
-                else:
-                    lines[v] = 1
-    print('Line names and number of stops:')
-    for k in lines:
-        print(f"bus_id: {k}, stops: {lines[k]}")
+    lines = {line['bus_id'] for line in dics}
+    stops = {s['stop_id']: s['stop_name'] for s in dics}
+    start = set()
+    finish = set()
+    for line in lines:
+        stop_s = [s['stop_name'] for s in dics if s['bus_id'] == line and s['stop_type'] == 'S']
+        stop_f = [s['stop_name'] for s in dics if s['bus_id'] == line and s['stop_type'] == 'F']
+        if len(stop_s) == 0 or len(stop_f) == 0:
+            print(f'There is no start or end stop for the line: {line}.')
+            return
+        start.add(stop_s[0])
+        finish.add(stop_f[0])
+    sd = {}
+    for d in dics:
+        sd.setdefault(d['stop_id'], []).append(d['bus_id'])
+    trans = [stops[s] for s in sd if len(sd[s]) > 1]
+    print(f'Start stops: {len(start)} {sorted(list(start))}')
+    print(f'Transfer stops: {len(trans)} {sorted(list(trans))}')
+    print(f'Finish  stops: {len(finish)} {sorted(list(finish))}')
 
 
 if __name__ == '__main__':
